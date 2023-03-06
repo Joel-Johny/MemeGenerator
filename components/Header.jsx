b2f1ed1d3f02e28039698ddcd3a75edc43a8a2c9
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-03-06 17:10:17 +0530

    updated directory structure and made nav bar
    updated directory structure and made nav bar

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header(){
+
+
+    return (
+        <nav className="Header">
+            <img src="../images/troll-face.png" className="nav-img"/>
+            <h2 className="title">Meme Generator</h2>
+            <h3 className="sub-title">React Mini Project</h3>
+        </nav>
+    )
+}
