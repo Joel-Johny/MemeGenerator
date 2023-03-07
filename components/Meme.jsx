1c8cfd2493ca3003fb1c66a427aceda6ca073867
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-03-07 12:38:25 +0530

    Made a input-form
    made input-form and added styling

---
+++ components/Meme.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Meme (){
+
+
+    return(
+        <div className="form-container">
+            <form method="post" action="/">
+                <input name="" className="input"></input>
+                <input name="" className="input"></input>
+                <button id="submit" type="submit">Get a new Image MEME!</button>
+            </form>
+        </div>
+
+    )
+}
