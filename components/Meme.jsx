9b28aa4c7133395895e54d4b463ca98e42db7b87
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-03-10 17:45:45 +0530

    Added onclick eventListener, meme container and meme data file
    Added onclick eventListener, meme container and meme data file

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,16 +1,30 @@
 import React from "react"
-
+import memesData from "../memesData"
 export default function Meme (){
 
 
+    const memesArray=memesData.data.memes
+    const [img,setImg]=React.useState("")
+    function generate(){
+        const random=Math.floor(Math.random()*memesArray.length)
+        const image=memesArray[random].url
+        setImg(image)
+    }
+
     return(
-        <div className="form-container">
-            <form method="post" action="/">
-                <input name="" className="input"></input>
-                <input name="" className="input"></input>
-                <button id="submit" type="submit">Get a new Image MEME!</button>
-            </form>
+
+        <div>
+
+        <div className="container">
+                <input name="" className="input" placeholder="Text1"></input>
+                <input name="" className="input" placeholder="Text2"></input>
+                <button id="submit" onClick={generate}>Get a new meme image !  🖼</button>
         </div>
 
+        <div className="meme-container">
+            <img src={img} className="meme-image"/>
+        </div>
+        
+        </div>
     )
 }
