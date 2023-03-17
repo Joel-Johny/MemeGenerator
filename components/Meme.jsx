c78a82a2bb1fc0655d331030e0ca7f98da6ddfe3
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-03-17 18:47:31 +0530

    added top and bottom text
    Added Top and bottom Text to image and also added onchange so that the state of the component is updated when a user interacts with the form.

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -3,26 +3,46 @@ import memesData from "../memesData"
 export default function Meme (){
 
 
-    const memesArray=memesData.data.memes
-    const [img,setImg]=React.useState("")
+    const [memeData,setMemeData]=React.useState({
+        top:"",
+        bottom:"",
+        image:"https://i.imgflip.com/30b1gx.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
     function generate(){
+        const memesArray=allMemeImages.data.memes
         const random=Math.floor(Math.random()*memesArray.length)
         const image=memesArray[random].url
-        setImg(image)
+        setMemeData((oldval)=>{
+            return {...oldval,image:image}
+        })
     }
 
+    function handleChange(event){
+        const {name,value}=event.target
+        setMemeData((old)=>{
+            return {
+                ...old,
+                [name]:value
+            }
+        })
+
+    }
     return(
 
         <div>
 
         <div className="container">
-                <input name="" className="input" placeholder="Text1"></input>
-                <input name="" className="input" placeholder="Text2"></input>
+                <input name="top" className="input" placeholder="Text1"  value={memeData.top} onChange={handleChange}></input>
+                <input name="bottom" className="input" placeholder="Text2" value={memeData.bottom} onChange={handleChange}></input>
                 <button id="submit" onClick={generate}>Get a new meme image !  🖼</button>
         </div>
 
         <div className="meme-container">
-            <img src={img} className="meme-image"/>
+            <img src={memeData.image} className="meme-image"/>
+            <h2 className="meme--text top">{memeData.top}</h2>
+            <h2 className="meme--text bottom">{memeData.bottom}</h2>
         </div>
         
         </div>
