4a3053c899c5711bb6236d383342218a00d1d7b8
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-03-22 01:13:38 +0530

    React Draggable,Meme Example.API import
    Using api to import meme,added button for meme example,installed React draggable

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,22 +1,27 @@
 import React from "react"
-import memesData from "../memesData"
 export default function Meme (){
 
 
     const [memeData,setMemeData]=React.useState({
         top:"",
         bottom:"",
-        image:"https://i.imgflip.com/30b1gx.jpg"
+        image:"https://i.imgflip.com/30b1gx.jpg",
+        name:"Drake Hotline Bling"
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMeme, setAllMeme] = React.useState("")
 
+    React.useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res=>res.json())
+        .then(data=>setAllMeme(data))
+    },[])
     function generate(){
-        const memesArray=allMemeImages.data.memes
+        const memesArray=allMeme.data.memes
         const random=Math.floor(Math.random()*memesArray.length)
         const image=memesArray[random].url
-        setMemeData((oldval)=>{
-            return {...oldval,image:image}
-        })
+        const name=memesArray[random].name
+        setMemeData( { top:"", bottom:"",image:image,name:name } )
+        
     }
 
     function handleChange(event){
@@ -28,6 +33,11 @@ export default function Meme (){
             }
         })
 
+    }
+    function newTab(){
+        const string=memeData.name+" meme example"
+        const urlOpen=`https://www.google.com/search?q=${encodeURIComponent(string)}&tbm=isch`
+        window.open(urlOpen, '_blank', 'noopener,noreferrer');
     }
     return(
 
@@ -43,7 +53,16 @@ export default function Meme (){
             <img src={memeData.image} className="meme-image"/>
             <h2 className="meme--text top">{memeData.top}</h2>
             <h2 className="meme--text bottom">{memeData.bottom}</h2>
+
+
         </div>
+
+        <div className="meme-name">
+            <h2>Meme Name : {memeData.name}</h2>
+        </div>
+        <button id="example" onClick={newTab}>Example</button>
+
+
         
         </div>
     )
